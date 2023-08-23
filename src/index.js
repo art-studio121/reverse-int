ee6579ea57f40ede666a0ea880cb6b4a01381309
Berdnikov Denis <art_studio121@inbox.ru>
2023-08-23 16:35:35 +0400

    feat: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0) return -1 * reverse(-n) * -1;
+    const solution = Number((n + "").split('').reverse().join(''));
+    return (solution > 2 ** 31 -1) ? 0 : solution;
 }
